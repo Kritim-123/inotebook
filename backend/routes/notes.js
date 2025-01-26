771150b5bc01c6f936163fb6fdfc532b35e73d00
Kritim-123 <kritimbastola2005@gmail.com>
2025-01-26 11:49:03 -0700

    Updating an exising Note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -48,13 +48,44 @@ noteRoute.post(
       const savedNote = await note.save();
 
       res.json(savedNote); // Sending the savedNotes
-
-
-    } catch (error) { // If we find any errors
+    } catch (error) {
+      // If we find any errors
       console.error(error);
       res.status(500).json({ error: "Server error" });
     }
   }
 );
+
+// ROUTE:3 Upate an exising note using: PUT "/api/notes/updatenote". Login required
+
+noteRoute.put("/updatenote/:id", fetchuser, async (req, res) => {
+  try {
+    const {title, description, tag} = req.body;
+
+    //Create a newNote object
+
+    const newNote = {};
+
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    // Find the note to be updated
+    let note = await Notes.findById(req.params.id);
+
+    if(!note){ return res.status(404),send("Not Found")}
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not allowed")
+    }
+
+    note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+    res.json({note});
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 export { noteRoute };
