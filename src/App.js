56ada407178622e3df9d5c9d1295297e7dfdd3be
Kritim-123 <kritimbastola2005@gmail.com>
2025-01-29 20:42:26 -0700

    Created NavBar and added react router dom to link components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
-function App(){
-  return(
-    <div className='App'>
-      <h1> This is inotebook</h1>
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-    </div>
-  )
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/about" element={<About />}></Route>
+
+          <Route path="/home" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
