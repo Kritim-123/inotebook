d7720b12118a13af0f5525770efc69c4bcfe8eea
Kritim-123 <kritimbastola2005@gmail.com>
2025-01-31 16:19:09 -0700

    implemented Context API to aceess elements

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Home from "./components/Home";
 import About from "./components/About";
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import NoteState from "./context/notes/NotesState";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
 
-        <Routes>
-          <Route path="/about" element={<About />}></Route>
+          <Routes>
+            <Route path="/about" element={<About />}></Route>
 
-          <Route path="/home" element={<Home />}></Route>
-        </Routes>
-      </BrowserRouter>
+            <Route path="/home" element={<Home />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </NoteState>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useContext, useEffect } from "react";
+import noteContext from "../context/notes/notesContext";
 
 const About = () => {
+
+  const a = useContext(noteContext);
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line 
+  }, []);
+
   return (
     <div>
-      This is about
+      This is about {a.state.name} and he is in class {a.state.class}
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
