4c4b886d49c5035ad4b81bc779fe0ce2c0a081cd
Kritim-123 <kritimbastola2005@gmail.com>
2025-01-31 21:22:07 -0700

    Added icons and fixed some minor errors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Alert from "./components/Alert";
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import NoteState from "./context/notes/NotesState";
@@ -12,6 +13,8 @@ function App() {
       <NoteState>
         <BrowserRouter>
           <Navbar />
+
+          <Alert message="Gather your notes here" />
           <div className="container">
             <Routes>
               <Route path="/about" element={<About />}></Route>
