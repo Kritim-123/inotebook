5e15d42404ef09dc7fa2eb547814d9ebfa6ba6c5
Kritim-123 <kritimbastola2005@gmail.com>
2025-02-05 20:27:43 -0700

    editing the notes in both client side and server side

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -5,7 +5,7 @@ import AddNote from "./AddNote";
 
 const Notes = () => {
   const context = useContext(noteContext);
-  const { notes, getNotes } = context;
+  const { notes, getNotes, editNote } = context;
 
   useEffect(() => {
     getNotes();
@@ -13,20 +13,28 @@ const Notes = () => {
   }, []);
 
   const ref = useRef(null);
+  // const refClose = useRef(null);
 
-  const [note, setNote] = useState({ etitle: "", edescription: "", etag: "" });
+  const [note, setNote] = useState({
+    id: "",
+    etitle: "",
+    edescription: "",
+    etag: "",
+  });
 
   const updateNote = (currentNote) => {
-
     ref.current.click();
 
-    setNote({etitle: currentNote.title, edescription: currentNote.description, etag : currentNote.tag})
+    setNote({
+      id: currentNote._id,
+      etitle: currentNote.title,
+      edescription: currentNote.description,
+      etag: currentNote.tag,
+    });
   };
 
   const handleClick = (e) => {
-    console.log("Updating the note..", note)
-    e.preventDefault();
-
+    editNote(note.id, note.etitle, note.edescription, note.etag);
   };
   const onChange = (e) => {
     setNote({ ...note, [e.target.name]: e.target.value });
@@ -81,7 +89,7 @@ const Notes = () => {
                     id="teitle"
                     name="etitle"
                     aria-describedby="emailHelp"
-                    value = {note.etitle}
+                    value={note.etitle}
                     onChange={onChange}
                   />
                 </div>
@@ -94,7 +102,7 @@ const Notes = () => {
                     name="edescription"
                     className="form-control"
                     id="edescription"
-                    value = {note.edescription}
+                    value={note.edescription}
                     onChange={onChange}
                   />
                 </div>
@@ -108,7 +116,7 @@ const Notes = () => {
                     name="etag"
                     className="form-control"
                     id="etag"
-                    value = {note.etag}
+                    value={note.etag}
                     onChange={onChange}
                   />
                 </div>
@@ -123,6 +131,7 @@ const Notes = () => {
                 Close
               </button>
               <button
+                data-bs-dismiss="modal"
                 type="button"
                 className="btn btn-primary"
                 onClick={handleClick}
--- src/context/notes/NotesState.js
+++ src/context/notes/NotesState.js
@@ -104,7 +104,7 @@ const NoteState = (props) => {
     // API CALL
 
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: "POST",
+      method: "PUT",
 
       headers: {
         "Content-Type": "application/json",
@@ -115,18 +115,26 @@ const NoteState = (props) => {
       body: JSON.stringify({title, description, tag}),
     });
 
-    const json = response.json();
+    const json = await response.json();
+    console.log (json)
+
+
+    let newNotes = JSON.parse(JSON.stringify(notes))
 
     //Logic to edit in Client
-    for (let index = (index = 0); index < Array.length; index++) {
-      const element = notes[index];
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
 
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+
+        break;
       }
     }
+
+    setNotes(newNotes);
   };
 
   return (
